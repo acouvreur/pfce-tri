240fbd05232deabc5c1e9fbaaad11aeefc06db9c
Ivan Picard Marchetto <ivan.picard-marchetto@etu.unice.fr>
2018-09-26 10:55:34 +0200

    fixed merge sort

--- tri.c
+++ tri.c
@@ -19,8 +19,19 @@ void triInsertion(int * array, int size){
 	}
 }
 
-
 void triFusion(int * array, int size){
+	if(size==1){ //Tableau de taille 1 deja trie
+		return;
+	}
+	int * buffer = malloc(sizeof(int)*size);
+	triFusion2(array,size,buffer);
+	for(int i=0;i<size;i++){
+		array[i]=buffer[i];
+	}
+}
+
+
+void triFusion2(int * array, int size, int * buffer){
 	if(size==1){ //Tableau de taille 1 deja trie
 		return;
 	}
@@ -28,42 +39,36 @@ void triFusion(int * array, int size){
 		//Coupe tableau en 2
 		int newSize1 = size/2;
 		int newSize2 = size/2 + size%2;
-		int * newArray1 = malloc(sizeof(int)*newSize1);
-		for(int i=0;i<newSize1;i++){
-			newArray1[i]=array[i];
-		}
-		for(int i=0;i<newSize2;i++){
-			newArray1[i]=array[i+newSize1];
-		}
-		int * newArray2 = malloc(sizeof(int)*newSize2);
+		
+		int * newArray1 = array;
+		int * newArray2 = &(array[newSize1]);
 		//Trie nouveau tableaux par recursion
-		triFusion(newArray1,newSize1);
-		triFusion(newArray2,newSize2);
+		triFusion2(newArray1,newSize1,buffer);
+		triFusion2(newArray2,newSize2,&(buffer[newSize1]));
 		//Fusion des 2 tableau trie
 		int i=0;int y=0;
 		while(i+y<size){
 			if(i<newSize1){
 				if(y<newSize2){
 					if(newArray1[i]<=newArray2[y]){
-						array[i+y] = newArray1[i];
+						buffer[i+y] = newArray1[i];
 						i++;
 					}
 					else{
-						array[i+y] = newArray2[y];
+						buffer[i+y] = newArray2[y];
 						y++;
 					}
 				}
 				else{
-					array[i+y] = newArray1[i];
-						i++;
+					buffer[i+y] = newArray1[i];
+					i++;
 				}
 				
 			}else{
-				array[i+y] = newArray2[y];
-						y++;
+				buffer[i+y] = newArray2[y];
+				y++;
 			}
 		}
-		free(newArray1);free(newArray2);
 	}
 
 }
--- tri.h
+++ tri.h
@@ -6,3 +6,4 @@ void triParTas(int * tree, int size);
 void tamiser(int * tree, node, int n);
 void sort(int * array);
 int comp (const void * elem1, const void * elem2);
+void triFusion2(int * array, int size, int * buffer);
